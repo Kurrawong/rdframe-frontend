1070b6d5fd6cfa7e22ddffa8d700fa01c3dd5989
david <dcchabgood@gmail.com>
2023-10-17 22:12:32 +1000

    Change remote endpoint

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
-    BACKEND_ENDPOINT: 'https://3knqdu7u25.execute-api.ap-southeast-2.amazonaws.com/production/'
-    // BACKEND_ENDPOINT: 'https://cjwuztc7m4st42247y3usiwcj40ifrul.lambda-url.ap-southeast-2.on.aws/'
+    // BACKEND_ENDPOINT: 'http://localhost:8000'
+    BACKEND_ENDPOINT: 'https://rdframe.sgraljii8d3km.ap-southeast-2.cs.amazonlightsail.com/'
 };

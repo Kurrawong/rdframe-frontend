8b0bb1e1a23e5091c69710e8769cdd0194ccbb2d
david <dcchabgood@gmail.com>
2023-10-17 22:14:12 +1000

    remove trailing slash

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
     // BACKEND_ENDPOINT: 'http://localhost:8000'
-    BACKEND_ENDPOINT: 'https://rdframe.sgraljii8d3km.ap-southeast-2.cs.amazonlightsail.com/'
+    BACKEND_ENDPOINT: 'https://rdframe.sgraljii8d3km.ap-southeast-2.cs.amazonlightsail.com'
 };
